fbcef29474ee5bd850cb6da3a43bfe996af93531
ysicing <i@ysicing.me>
2022-12-14 15:19:28 +0800

    feat(update): fix pod install
    fix pod install
    
    Signed-off-by: ysicing <i@ysicing.me>

--- hack/tools/podinstall/entrypoint.sh
+++ hack/tools/podinstall/entrypoint.sh
@@ -10,11 +10,10 @@ export APP_DOMAIN=${APP_DOMAIN:-k3s.local}
 export APP_TOKEN=${APP_TOKEN:-XZdrjxhAhq5pDjpEU3kR4djsvJ3rfj0M}
 export TOP_DOMAIN=${APP_DOMAIN#*.}
 
-kubectl apply -f  https://pkg.qucheng.com/ssl/${TOP_DOMAIN}/${APP_DOMAIN}/tls.yaml
-
 cat > /tmp/qucheng.yaml <<EOF
 cloud:
   defaultChannel: stable
+  apphttpsPort: ${APP_HTTPS_PORT}
 env:
   APP_DOMAIN: ${APP_DOMAIN}
   CNE_API_TOKEN: ${APP_TOKEN}
@@ -49,3 +48,27 @@ worker: null
 EOF
 
 [ -f "/qcli/k3syaml/k3s.yaml" ] && cp -a /qcli/k3syaml/k3s.yaml /qcli/root/.kube/config
+
+# wait tls ok
+wait_for_tls() {
+    local retries=${MAXWAIT:-300}
+    echo "check the tls is available."
+    for ((i = 1; i <= $retries; i += 1)); do
+        code=$(curl -s -o /dev/null -w "%{http_code}" https://pkg.qucheng.com/ssl/${TOP_DOMAIN}/${APP_DOMAIN}/tls.yaml)
+        if [ $code != 404 ] ;
+        then
+            echo "tls is ready."
+            break
+        fi
+
+        echo "Waiting tls gen $i seconds"
+        sleep 1
+        if [ "$i" == "$retries" ]; then
+            echo "unable to load tls"
+            return 1
+        fi
+    done
+    return 0
+}
+
+wait_for_tls && kubectl apply -f https://pkg.qucheng.com/ssl/${TOP_DOMAIN}/${APP_DOMAIN}/tls.yaml || echo "load tls failed"
