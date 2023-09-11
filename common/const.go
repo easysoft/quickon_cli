528716f1bf2cf34d378539ebc14ee9fd977870d6
ysicing <i@ysicing.me>
2023-09-11 16:28:59 +0800

    fix(docs): fix devops docs url
    fix devops docs url
    
    Signed-off-by: ysicing <i@ysicing.me>

--- common/const.go
+++ common/const.go
@@ -114,7 +114,7 @@ const (
 const (
 	DownloadAgent      = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4688.0 Safari/537.36"
 	QuchengDocs        = "https://www.qucheng.com"
-	ZentaoDocs         = "https://www.zentao.net/book/zentaopms/38.html"
+	ZentaoDocs         = "https://www.zentao.net/book/devops.html"
 	QuchengDefaultUser = "qadmin"
 	QuchengDefaultPass = "pass4Quickon"
 )
