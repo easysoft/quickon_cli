2818d07f0f6cc415b48e961439983b104a869dcc
ysicing <i@ysicing.me>
2025-03-25 10:00:27 +0800

    feat: improve MySQL connection management and error handling
    - Add `database/sql` and `fmt` imports for MySQL connection handling
    - Introduce command-line flags for host, port, username, and password
    - Implement MySQL connection test with feedback logging
    - Add logic to create and drop a test database within the command execution flow
    - Enhance error handling and logging for MySQL operations
    
    Signed-off-by: ysicing <i@ysicing.me>

--- internal/pkg/cli/check/mysql.go
+++ internal/pkg/cli/check/mysql.go
@@ -7,6 +7,9 @@
 package check
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/spf13/cobra"
 
 	"github.com/easysoft/qcadmin/internal/pkg/util/factory"
@@ -14,5 +17,50 @@ import (
 
 // CheckMySQLCommand check mysql command.
 func CheckMySQLCommand(f factory.Factory) *cobra.Command {
-	return nil
+	var (
+		host     string
+		port     int
+		username string
+		password string
+	)
+	cmd := &cobra.Command{
+		Use:   "mysql",
+		Short: "check mysql availability and database creation",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if password == "" {
+				return fmt.Errorf("password is required")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			// Test MySQL connection
+			f.GetLog().Infof("test mysql connection %:%d", host, port)
+			db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/", username, password, host, port))
+			if err != nil {
+				f.GetLog().Errorf("failed to connect to mysql(%s:%d), err: %v", host, port, err)
+				return
+			}
+			defer db.Close()
+			// Test database creation
+			f.GetLog().Infof("try create database z_test_db")
+			_, err = db.Exec("CREATE DATABASE z_test_db")
+			if err != nil {
+				f.GetLog().Errorf("failed to create database z_test_db,err: %v", err)
+				return
+			}
+			f.GetLog().Infof("clean test database z_test_db")
+			_, err = db.Exec("DROP DATABASE z_test_db")
+			if err != nil {
+				f.GetLog().Errorf("failed to drop test database: %v", err)
+				return
+			}
+			f.GetLog().Done("mysql is available and can create databases")
+			return
+		},
+	}
+	cmd.Flags().StringVarP(&host, "host", "h", "localhost", "MySQL host")
+	cmd.Flags().IntVarP(&port, "port", "", 3306, "MySQL port")
+	cmd.Flags().StringVarP(&username, "username", "u", "root", "MySQL username")
+	cmd.Flags().StringVarP(&password, "password", "p", "", "MySQL password")
+	return cmd
 }
